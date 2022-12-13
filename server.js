111c1af084c1293902ed42d0214237d393a91a20
ParasPachore <paraspachore@gmail.com>
2022-12-13 17:15:54 +0530

    commit

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const http = require("http");
 
 const port = 8081;
 
+// http methods
+// GET  --> Getting details from server
+// POST --> Sending data to server
+// PUT --> Overwrite / fully update
+// DELETE --> delete data from server
+// PATCH --> Update very few fields / certain fields
+
 http
     .createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/HTML" });
@@ -14,4 +21,11 @@ http
         console.log('My nodeJS server started on port ${port}');
     });
 
+const todoList = ["need to learn", "need to code"];
+
+
 // http://localhost:8081
+// http://localhost:8081/
+// http://localhost:8081/home
+// http://localhost:8081/abouUS
+// http://localhost:8081/contactUS
